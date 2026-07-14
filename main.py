58134e5ba3dc72d991639305491ea494dd716180
Shakalen Shaikh <160219957+ShakalBhau0001@users.noreply.github.com>
2026-07-14 13:08:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -75,20 +75,15 @@ def morse_to_text(morse):
 
 ctk.set_appearance_mode("dark")
 ctk.set_default_color_theme("blue")
-
 app = ctk.CTk()
 app.title("Morse Decoder CLI → GUI Lab")
 app.geometry("700x500")
-
-
 title = ctk.CTkLabel(
     app, text="🔤 Morse Decoder GUI Lab", font=("Consolas", 22, "bold")
 )
 title.pack(pady=10)
-
 input_box = ctk.CTkTextbox(app, height=120, font=("Consolas", 14))
 input_box.pack(padx=20, pady=10, fill="x")
-
 output_box = ctk.CTkTextbox(app, height=120, font=("Consolas", 14))
 output_box.pack(padx=20, pady=10, fill="x")
 
@@ -107,13 +102,10 @@ def decode_action():
 
 button_frame = ctk.CTkFrame(app)
 button_frame.pack(pady=15)
-
 encode_btn = ctk.CTkButton(button_frame, text="Text → Morse", command=encode_action)
 encode_btn.grid(row=0, column=0, padx=15)
-
 decode_btn = ctk.CTkButton(button_frame, text="Morse → Text", command=decode_action)
 decode_btn.grid(row=0, column=1, padx=15)
-
 footer = ctk.CTkLabel(
     app, text="CLI Logic • GUI Layer • Learning Lab", font=("Consolas", 12)
 )
